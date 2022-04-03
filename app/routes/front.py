9b08a52c5e84809c2fe6b421142b5d46fc418930
Tyler Perkins <tperki17@kent.edu>
2022-04-03 18:53:32 -0400

    Add API routes for documents

--- app/routes/front.py
+++ app/routes/front.py
@@ -10,3 +10,32 @@ front = Blueprint("front", __name__)
 def home():
     return homecontroller.index()
 
+@front.route('/instructor', methods=['GET', 'POST'])
+def instructor():
+    return render_template("instructor.html",
+        defaultFields = defaultFields,
+        fieldsLen = len(defaultFields),
+        testCourses = testCourses,
+        courseLen = len(testCourses)
+    )
+
+@front.route('/administrator', methods=['GET', 'POST'])
+def administrator():
+    return render_template("administrator.html",
+        requiredFields = requiredFields,
+        fieldsLen = len(requiredFields)
+    )
+
+@front.route('/scheduler', methods=['GET', 'POST'])
+def scheduler():
+    return render_template("scheduler.html"
+    )
+
+@front.route('/document/docx/<CRN>', methods=['GET'])
+def document(CRN):
+    return 'Hello from document! ' + CRN
+
+@front.route('/document/excel/', methods=['POST'])
+def excel():
+    f = request.files['file']
+    return 'file uploaded'
