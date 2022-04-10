97c258b65e103091045d39930fa3fe21517df2ff
Tyler Perkins <tperki17@kent.edu>
2022-04-10 15:30:33 -0400

    Update setup to reflect new pymongo dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
-        'Flask-PyMongo',
+        'pymongo',
         'config',
         'python-docx',
         'pandas',
