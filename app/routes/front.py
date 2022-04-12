65c77112271e54aa2450306f0d6caa8bdb9f2d54
Amrit Shenava <amritshenava@outlook.com>
2022-04-12 12:35:07 -0400

    auth functions

--- app/routes/front.py
+++ app/routes/front.py
@@ -1,10 +1,13 @@
 from flask import Blueprint,request,json
 from flask_pymongo import ObjectId
 from ..app import mongo
+from ..app import waffleauth
+from waffleauth import signin
 from flask import render_template
 from app.controllers.HomeController import homecontroller
 from app.controllers.DocumentController import documentcontroller
 from app.helpers.Utility import sendResponse
+
 
 front = Blueprint("front", __name__)
 
@@ -79,6 +82,12 @@ def scheduler():
 @front.route('/signin', methods=['GET', 'POST'])
 def signin():
     return render_template("signin.html")
+def signinFunc():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('email')
+        return signin(email, password)
+    return render_template('/administrator.html')
 
 @front.route('/document/docx/<CRN>', methods=['GET'])
 def document(CRN):
