e490ceb091527ba0508272309ef0c67b725b3f2f
Tyler Perkins <tperki17@kent.edu>
2022-04-14 14:54:24 -0400

    Add Dockerfile and related tools

--- app/models/Course.py
+++ app/models/Course.py
@@ -1,7 +1,6 @@
 from ..app import mongo
 from flask import request,json
 import pprint
-from flask_pymongo import ObjectId
 
 class Course():
     def __init__(self):
---
+++ run-docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build --tag waffleiron .
+docker-compose up
