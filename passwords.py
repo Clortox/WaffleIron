ad6e91335589030e00dd8e3999e25b7da78ebd79
bbehnkeSE <bbehnke@kent.edu>
2022-04-15 20:37:20 -0400

    Cleaned up password file

--- passwords.py
+++ passwords.py
@@ -8,13 +8,8 @@ def encode_password(password):
 	hash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
 	hash = binascii.hexlify(hash)
 	hash = (salt + hash).decode('ascii')
+	
 	return hash
-
-
-def test_encode_password():
-	hash = encode_password("mypassword")
-	assert type(hash) is str
-	assert len(hash) > 60
 
 
 def verify_password(password, hash):
@@ -25,26 +20,3 @@ def verify_password(password, hash):
 	new_hash = new_hash.decode('ascii')
 
 	return old_hash == new_hash
-
-
-def test_verify_password():
-    hash = encode_password("mypassword")
-    result = verify_password("mypassword", hash)
-    assert result, "Correct password does not verify"
-    result = verify_password("xmypassword", hash)
-    assert not result, "Incorrect password does improperly verify"
-    result = verify_password("mypassxword", hash)
-    assert not result, "Incorrect password does improperly verify"
-    result = verify_password("mypasswordx", hash)
-    assert not result, "Incorrect password does improperly verify"
-    result = verify_password("mypassWord", hash)
-    assert not result, "Incorrect password does improperly verify"
-    result = verify_password("cat", hash)
-    assert not result, "Incorrect password does improperly verify"
-
-
-
-if __name__ == "__main__":
-    test_encode_password()
-    test_verify_password()
-    print("Done.")
