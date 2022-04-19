fdda4b6f93d61a3b6e0f9ed2f2bfef9427114a80
bbehnkeSE <bbehnke@kent.edu>
2022-04-19 09:14:31 -0400

    added config info

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ import config
 import os
 
 app = Flask(__name__, template_folder="views")
+app.secret_key = b'\xa0\x82\x86r\x8e \xb7RR\xf4\x8cw\xeax\xa7\x90\xec\xc9*\xb6\x97\xb5\x80\xa7\x82m\x88\xc4\xf7\xb51\x96'
+
 #load correct config
 if os.environ['FLASK_DEBUG'] == 1:
     cfg = config.Config(os.path.join(os.path.dirname(__file__),'debug.cfg'))
